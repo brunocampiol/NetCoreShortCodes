7f3939a9d00b08af2e7bc6135e2f7c24ea92f2d5
Bruno Paes <bruno.paes@adp.com>
2023-02-14 12:56:33 -0300

    add benchmark csproj

---
+++ src/NetCoreShortCodes.Benchmark/LoopIteration.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace NetCoreShortCodes.Benchmark
+{
+    public class LoopIteration
+    {
+        private const int N = 10000;
+        private readonly List<string> stringList;
+
+        public LoopIteration()
+        {
+            stringList = new List<string>();
+            var random = new Random(420);
+            for (int i = 0; i < N; i++)
+            {
+                var randomInteger = random.Next();
+                stringList.Add(randomInteger.ToString());
+            }
+        }
+
+        [Benchmark]
+        public void ForIteration()
+        {
+            for (int i = 0; i < stringList.Count; i++)
+            {
+
+            }
+        }
+
+        [Benchmark]
+        public void ForIterationWithCountMethod()
+        {
+            for (int i = 0; i < stringList.Count(); i++)
+            {
+
+            }
+        }
+
+        [Benchmark]
+        public void ForEach()
+        {
+            foreach (var item in stringList)
+            {
+
+            }
+        }
+
+        [Benchmark]
+        public void ForEachLinq()
+        {
+            stringList.ForEach(x => { });
+        }
+    }
+}
---
+++ src/NetCoreShortCodes.Benchmark/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Running;
+using NetCoreShortCodes.Benchmark;
+
+//var summary = BenchmarkRunner.Run<Md5VsSha256>();
+BenchmarkRunner.Run<LoopIteration>();
+BenchmarkRunner.Run<Md5VsSha256>();
