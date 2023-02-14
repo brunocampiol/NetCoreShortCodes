a392aa2ac487c680a3ba0bb1e2313d4d55022be4
Bruno Paes <bruno.paes@adp.com>
2023-02-14 17:20:33 -0300

    add asyncawait csproj

---
+++ src/NetCoreShortCodes.AsyncAwait/AsyncClass.cs
@@ -0,0 +1,44 @@
+﻿namespace NetCoreShortCodes.AsyncAwait
+{
+    internal static class AsyncClass
+    {
+        // Because of the way async/await methods are rewritten by the compiler,
+        // any exceptions thrown during the parameters check will happen only
+        // when the task is observed.That could happen far away from the
+        // source of the buggy code or never happen for fire-and-forget tasks.
+
+        // Therefore it is recommended to split the method into two: an outer
+        // method handling the parameter checks (without being async/await)
+        // and an inner method to handle the iterator block with the
+        // async/await pattern.
+
+        public static async Task SkipLinesNonCompliantAsync(this TextReader reader, int linesToSkip) 
+        {
+            if (reader == null) { throw new ArgumentNullException(nameof(reader)); }
+            if (linesToSkip < 0) { throw new ArgumentOutOfRangeException(nameof(linesToSkip)); }
+
+            for (var i = 0; i < linesToSkip; ++i)
+            {
+                var line = await reader.ReadLineAsync().ConfigureAwait(false);
+                if (line == null) { break; }
+            }
+        }
+
+        public static Task SkipLinesCompliantAsync(this TextReader reader, int linesToSkip)
+        {
+            if (reader == null) { throw new ArgumentNullException(nameof(reader)); }
+            if (linesToSkip < 0) { throw new ArgumentOutOfRangeException(nameof(linesToSkip)); }
+
+            return reader.SkipLinesInternalAsync(linesToSkip);
+        }
+
+        private static async Task SkipLinesInternalAsync(this TextReader reader, int linesToSkip)
+        {
+            for (var i = 0; i < linesToSkip; ++i)
+            {
+                var line = await reader.ReadLineAsync().ConfigureAwait(false);
+                if (line == null) { break; }
+            }
+        }
+    }
+}
---
+++ src/NetCoreShortCodes.AsyncAwait/Program.cs
@@ -0,0 +1,15 @@
+﻿using NetCoreShortCodes.AsyncAwait;
+
+// No difference - throws exception
+//await AsyncClass.SkipLinesNonCompliantAsync(null, 0);
+//await AsyncClass.SkipLinesCompliantAsync(null, 0);
+
+// Fire and forget, compliant solution ONLY throws exception
+//_ = AsyncClass.SkipLinesNonCompliantAsync(null, 0);
+//_ = AsyncClass.SkipLinesCompliantAsync(null, 0);
+
+// Throws exception
+var tasks = new List<Task>();
+tasks.Add(AsyncClass.SkipLinesNonCompliantAsync(null, 0));
+tasks.Add(AsyncClass.SkipLinesCompliantAsync(null, 0));
+await Task.WhenAll(tasks);
