83b4de8dae2c8a42c623cdc7beedf97a9951ede6
bcpaes <bruno.campiol@gmail.com>
2023-06-27 14:34:07 -0300

    Feature/sqlite api (#4)
    * initial version user repository -with erros
    
    * fix sql matching parameters
    
    * add date created
    
    * crud implementation for user
    
    ---------
    
    Co-authored-by: Bruno Paes <bruno.paes@adp.com>

--- src/NetCoreShortCodes.API/Database/DatabaseInitializer.cs
+++ src/NetCoreShortCodes.API/Database/DatabaseInitializer.cs
@@ -13,13 +13,14 @@ namespace NetCoreShortCodes.API.Database
 
         public async Task InitializeAsync()
         {
+            // Id CHAR(36) PRIMARY KEY,
+            // DateOfBirth TEXT,
             using var connection = await _connectionFactory.CreateConnectionAsync();
-            await connection.ExecuteAsync(@"CREATE TABLE IF NOT EXISTS Customers (
-                                            Id CHAR(36) PRIMARY KEY, 
+            await connection.ExecuteAsync(@"CREATE TABLE IF NOT EXISTS Users (              
+                                            IsActive INTEGER NOT NULL,
+                                            Karma INTEGER NOT NULL,
                                             Username TEXT NOT NULL,
-                                            FullName TEXT NOT NULL,
-                                            Email TEXT NOT NULL,
-                                            DateOfBirth TEXT NOT NULL)");
+                                            DateCreated TEXT NOT NULL)");
 
             await connection.ExecuteAsync(@"CREATE TABLE IF NOT EXISTS DbEntity (
                                             Id INTEGER PRIMARY KEY,
