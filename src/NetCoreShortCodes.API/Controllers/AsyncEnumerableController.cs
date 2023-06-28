3cfbe346913d4412372bc10bb0e351647fa65a4d
Bruno Paes <bruno.paes@adp.com>
2023-06-28 13:36:40 -0300

    stream controller

---
+++ src/NetCoreShortCodes.API/Controllers/AsyncEnumerableController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreShortCodes.API.Models;
+using NetCoreShortCodes.API.Services;
+
+namespace NetCoreShortCodes.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class AsyncEnumerableController : ControllerBase
+    {
+        // The IAsyncEnumerable as of 2023-06-28 does not work on swagger ui / postman.
+        // To see the results as streams, hit the endpoint with the browser
+        // TODO fix IAsyncEnumerable on postman / swagger ui
+
+        /// <summary>
+        /// Returns an async stream of integers
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IAsyncEnumerable<int> Get() => AsyncEnumerableService.FetchAsyncEnumerableOfIntegers();
+
+        /// <summary>
+        /// Returns an async stream of integers
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("weather")]
+        public IAsyncEnumerable<WeatherForecast> GetWeather() => AsyncEnumerableService.FetchAsyncEnumerableOfWeatherForecast();
+    }
+}
---
+++ src/NetCoreShortCodes.API/Services/AsyncEnumerableService.cs
@@ -0,0 +1,58 @@
+﻿using NetCoreShortCodes.API.Models;
+
+namespace NetCoreShortCodes.API.Services
+{
+    public static class AsyncEnumerableService
+    {
+        public static async IAsyncEnumerable<int> FetchAsyncEnumerableOfIntegers()
+        {
+            var random = new Random();
+            var maxItems = random.Next(100, 10_000);
+
+            for (int i = 1; i <= maxItems; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+
+        public static async IAsyncEnumerable<WeatherForecast> FetchAsyncEnumerableOfWeatherForecast()
+        {
+            var random = new Random();
+            var maxItems = random.Next(100, 10_000);
+
+            for (int i = 1; i <= maxItems; i++)
+            {
+                await Task.Delay(100);
+                var forecast = new WeatherForecast()
+                {
+                    TemperatureC = random.Next(-50, 50)
+                };
+
+                switch (forecast.TemperatureC)
+                {
+                    case < 0:
+                        forecast.Summary = "Very cold";
+                        break;
+                    case < 16:
+                        forecast.Summary = "Cold";
+                        break;
+                    case < 26:
+                        forecast.Summary = "Warm";
+                        break;
+                    case < 36:
+                        forecast.Summary = "Hot";
+                        break;
+                    case < 46:
+                        forecast.Summary = "Boiling";
+                        break;
+                    default:
+                        forecast.Summary = "Sweltering";
+                        break;
+                }
+
+                yield return forecast;
+            }
+        }
+    }
+}
