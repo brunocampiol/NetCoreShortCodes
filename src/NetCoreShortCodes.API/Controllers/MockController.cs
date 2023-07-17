5eaaa76977b3bc3cec35edcb868dd799ba7f1a9c
Bruno Paes <bruno.paes@adp.com>
2023-07-17 13:37:38 -0300

    adding mock controller

---
+++ src/NetCoreShortCodes.API/Controllers/MockController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreShortCodes.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class MockController : ControllerBase
+    {
+        public MockController()
+        {
+            
+        }
+
+        /// <summary>
+        /// Mocks a response and return based on inputs
+        /// </summary>
+        /// <param name="delayMilliseconds">Optional: waits for the specified milliseconds</param>
+        /// <param name="customResponse">Optional: custom message to return</param>
+        /// <param name="statusCode">The HTTP status code to return</param>
+        /// <returns></returns>
+        [HttpGet("{statusCode}")]
+        public async Task<IActionResult> Get([FromHeader] int? delayMilliseconds,
+                                             [FromHeader] string? customResponse,
+                                             int statusCode)
+        {
+            if (delayMilliseconds.HasValue)
+            {
+                await Task.Delay(delayMilliseconds.Value);
+            }
+
+            return StatusCode(statusCode, customResponse);
+        }
+    }
+}
