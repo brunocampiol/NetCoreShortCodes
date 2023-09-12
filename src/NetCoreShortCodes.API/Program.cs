2dd45f4354430ffdd0f76e4bea75e1eff1a06adc
Bruno Paes <bruno.campiol@gmail.com>
2023-09-11 21:32:10 -0300

    updating openapi types in swagger

--- src/NetCoreShortCodes.API/Program.cs
+++ src/NetCoreShortCodes.API/Program.cs
@@ -25,15 +25,25 @@ builder.Services.AddSwaggerGen(options =>
     });
     options.MapType<TimeOnly>(() => new OpenApiSchema
     {
-        // TODO: add pattern here
         Type = "string",
-        Example = new OpenApiString(DateTime.Now.ToString("HH:mm:ss.fffffff"))
+        Pattern = "^(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:\\.\\d{1,7})?$"
+        //Example = new OpenApiString(DateTime.Now.ToString("HH:mm:ss.fffffff"))
     });
     options.MapType<TimeSpan>(() => new OpenApiSchema
     {
-        // TODO: add pattern here
         Type = "string",
-        Example = new OpenApiString(DateTime.Now.ToString("1.HH:mm:ss.fffffff"))
+        Pattern = "^(\\d{1,7}\\.)?([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)(?:\\.(\\d{1,7}))?$"
+        //Example = new OpenApiString(DateTime.Now.ToString("d.HH:mm:ss.fffffff"))
+    });
+    options.MapType<DateTime>(() => new OpenApiSchema
+    {
+        Type = "string",
+        Example = new OpenApiString(DateTimeOffset.Now.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF"))
+    });
+    options.MapType<DateTimeOffset>(() => new OpenApiSchema
+    {
+        Type = "string",
+        Example = new OpenApiString(DateTimeOffset.Now.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFFzzz"))
     });
     options.SwaggerDoc("v1", new OpenApiInfo
     {
