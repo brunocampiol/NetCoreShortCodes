824299c092dc40c2b6f90fd1c43f38d6f401e36a
Bruno Paes <bruno.paes@adp.com>
2023-09-14 17:58:16 -0300

    moving swagger config to static class

---
+++ src/NetCoreShortCodes.API/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace NetCoreShortCodes.API.Configuration
+{
+    public static class SwaggerConfiguration
+    {
+        internal static void AddAppSwaggerGen(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.MapType<char>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    MinLength = 1,
+                    MaxLength = 1
+                });
+                options.MapType<TimeOnly>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    Pattern = "^(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:\\.\\d{1,7})?$",
+                    Example = new OpenApiString(DateTime.Now.ToString("HH:mm:ss.fffffff"))
+                });
+                options.MapType<TimeSpan>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    Pattern = "^(\\d{1,7}\\.)?([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)(?:\\.(\\d{1,7}))?$",
+                    Example = new OpenApiString(DateTime.Now.ToString("d.HH:mm:ss.fffffff"))
+                });
+                options.MapType<DateTime>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    Example = new OpenApiString(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF"))
+                });
+                options.MapType<DateTimeOffset>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    Example = new OpenApiString(DateTimeOffset.Now.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFFzzz"))
+                });
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Example API",
+                    Description = "Example API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "BrunoCampiol",
+                        Url = new Uri("https://brunocampiol.github.io/about.html")
+                    }
+                });
+                var xmlFilename = $"{typeof(Program).Assembly.GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+        }
+    }
+}
--- src/NetCoreShortCodes.API/Program.cs
+++ src/NetCoreShortCodes.API/Program.cs
@@ -1,8 +1,7 @@
 using Dapper;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.OpenApi.Any;
-using Microsoft.OpenApi.Models;
+using NetCoreShortCodes.API.Configuration;
 using NetCoreShortCodes.API.Database;
 using NetCoreShortCodes.API.Repositories;
 using NetCoreShortCodes.API.Repositories.Dapper;
@@ -14,51 +13,7 @@ var config = builder.Configuration;
 builder.Services.AddHealthChecks()
                 .AddSqlite(config["Database:ConnectionString"]);
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // only required for minimal APIs
-builder.Services.AddSwaggerGen(options =>
-{
-    options.MapType<char>(() => new OpenApiSchema
-    {
-        Type = "string",
-        MinLength = 1,
-        MaxLength = 1
-    });
-    options.MapType<TimeOnly>(() => new OpenApiSchema
-    {
-        Type = "string",
-        Pattern = "^(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:\\.\\d{1,7})?$",
-        Example = new OpenApiString(DateTime.Now.ToString("HH:mm:ss.fffffff"))
-    });
-    options.MapType<TimeSpan>(() => new OpenApiSchema
-    {
-        Type = "string",
-        Pattern = "^(\\d{1,7}\\.)?([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)(?:\\.(\\d{1,7}))?$",
-        Example = new OpenApiString(DateTime.Now.ToString("d.HH:mm:ss.fffffff"))
-    });
-    options.MapType<DateTime>(() => new OpenApiSchema
-    {
-        Type = "string",
-        Example = new OpenApiString(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF"))
-    });
-    options.MapType<DateTimeOffset>(() => new OpenApiSchema
-    {
-        Type = "string",
-        Example = new OpenApiString(DateTimeOffset.Now.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFFzzz"))
-    });
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Example API",
-        Description = "Example API",
-        Contact = new OpenApiContact
-        {
-            Name = "BrunoCampiol",
-            Url = new Uri("https://brunocampiol.github.io/about.html")
-        }
-    });
-    var xmlFilename = $"{typeof(Program).Assembly.GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+builder.Services.AddAppSwaggerGen();
 
 builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
     new SqliteConnectionFactory(config["Database:ConnectionString"]));
@@ -75,7 +30,7 @@ SqlMapper.AddTypeHandler(new TimeSpanHandler());
 
 var app = builder.Build();
 
-// Register middlewares (middlwares are sequential)
+// Register middlewares (order matters!)
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
